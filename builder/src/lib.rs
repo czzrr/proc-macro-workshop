d1f2effa1743309e72f4b69c3ebc014d706596f2
Christopher Acosta <chriz@live.dk>
2023-07-11 17:31:51 +0200

    Builder pt. 1-5

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,73 @@
 use proc_macro::TokenStream;
+use proc_macro2::Span;
+use syn::{parse_macro_input, DeriveInput, Ident, Data};
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+    let builder_name = Ident::new(&format!("{}Builder", name), Span::call_site());
+    let fields = match input.data {
+        Data::Struct(s) => s.fields,
+        _ => panic!("Only structs are supported"),
+    };
+    // let fields_named = match fields {
+    //     Fields::Named(f) => f,
+    //     _ => panic!("Only structs with named fields are supported"),
+    // };
 
-    unimplemented!()
+    let builder_field_decls = fields.iter().map(|field| {
+        let field_ident = field.ident.as_ref().unwrap();
+        let field_ty = &field.ty;
+        quote!(#field_ident: Option<#field_ty>)
+    });
+
+    let builder_fields_init = fields.iter().map(|field| {
+        let field_ident = field.ident.as_ref().unwrap();
+        quote!(#field_ident: None)
+    });
+
+    let setters = fields.iter().map(|field| {
+        let field_ident = field.ident.as_ref().unwrap();
+        let field_ty = &field.ty;
+        quote!(pub fn #field_ident(&mut self, #field_ident: #field_ty) -> &mut Self {
+            self.#field_ident = Some(#field_ident);
+            self
+        })
+    });
+
+    let build_checks = fields.iter().map(|field| {
+        let field_ident = field.ident.as_ref().unwrap();
+        let field_ident_str = field_ident.to_string();
+        quote!(
+            let #field_ident = self.#field_ident.clone().ok_or(format!("field `{}` not set", #field_ident_str))?;
+        )
+    });
+
+    let build = fields.iter().map(|field| field.ident.as_ref().unwrap());
+
+    let expanded = quote! {
+        pub struct #builder_name {
+            #(#builder_field_decls),*
+        }
+        impl #builder_name {
+            pub fn build(&mut self) -> Result<#name, Box<dyn std::error::Error>> {
+                #(#build_checks)*
+                Ok(#name {
+                    #(#build),*
+                })
+            }
+            #(#setters)*
+        }
+        impl #name {
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    #(#builder_fields_init),*
+                }
+            }
+        }
+    };
+    expanded.into()
+    //unimplemented!()
 }
