9cd792d18d0799076f8d64f7b9b4e06f1db2b362
Christopher Acosta <chriz@live.dk>
2023-07-11 20:18:20 +0200

    Builder pt. 6

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,9 +1,9 @@
 use proc_macro::TokenStream;
 use proc_macro2::Span;
-use syn::{parse_macro_input, DeriveInput, Ident, Data};
+use syn::{parse_macro_input, DeriveInput, Ident, Data, GenericArgument, AngleBracketedGenericArguments, PathArguments, PathSegment, Path, TypePath, Type, punctuated::Pair};
 use quote::quote;
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = input.ident;
@@ -20,7 +20,11 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let builder_field_decls = fields.iter().map(|field| {
         let field_ident = field.ident.as_ref().unwrap();
         let field_ty = &field.ty;
-        quote!(#field_ident: Option<#field_ty>)
+        if is_option_type(field_ty).is_some() {
+            quote!(#field_ident: #field_ty)
+        } else {
+            quote!(#field_ident: Option<#field_ty>)
+        }
     });
 
     let builder_fields_init = fields.iter().map(|field| {
@@ -31,18 +35,33 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let setters = fields.iter().map(|field| {
         let field_ident = field.ident.as_ref().unwrap();
         let field_ty = &field.ty;
-        quote!(pub fn #field_ident(&mut self, #field_ident: #field_ty) -> &mut Self {
-            self.#field_ident = Some(#field_ident);
-            self
-        })
+        
+        match is_option_type(field_ty) {
+            Some(ty) => quote!(pub fn #field_ident(&mut self, #field_ident: #ty) -> &mut Self {
+                self.#field_ident = Some(#field_ident);
+                self
+            }),
+            _ => quote!(pub fn #field_ident(&mut self, #field_ident: #field_ty) -> &mut Self {
+                self.#field_ident = Some(#field_ident);
+                self
+            })
+        }
     });
 
     let build_checks = fields.iter().map(|field| {
         let field_ident = field.ident.as_ref().unwrap();
+        let field_ty = &field.ty;
         let field_ident_str = field_ident.to_string();
-        quote!(
-            let #field_ident = self.#field_ident.clone().ok_or(format!("field `{}` not set", #field_ident_str))?;
-        )
+
+        if is_option_type(field_ty).is_some() {
+            quote!(
+                let #field_ident = self.#field_ident.clone();
+            )
+        } else {
+            quote!(
+                let #field_ident = self.#field_ident.clone().ok_or(format!("field `{}` not set", #field_ident_str))?;
+            )
+        }
     });
 
     let build = fields.iter().map(|field| field.ident.as_ref().unwrap());
@@ -70,4 +89,36 @@ pub fn derive(input: TokenStream) -> TokenStream {
     };
     expanded.into()
     //unimplemented!()
+}
+
+fn is_option_type(field_ty: &Type) -> Option<Type> {
+    let segments = match field_ty {
+        Type::Path(
+            TypePath {
+                qself: None,
+                path: Path {
+                    segments, ..
+                },
+            },
+        ) => segments,
+        _ => return None,
+    };
+    
+    let generic_args = match segments.pairs().next() {
+        Some(Pair::End(
+            PathSegment {
+                ident: i,
+                arguments: PathArguments::AngleBracketed(
+                    AngleBracketedGenericArguments {
+                        args, ..
+                    },
+                ),
+            })) if i.to_string() == "Option" => args,
+        _ => return None,
+    };
+
+    match generic_args.pairs().next() {
+        Some(Pair::End(GenericArgument::Type(ty))) => Some(ty.clone()),
+        _ => None
+    }
 }
