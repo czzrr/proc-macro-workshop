17c4b138e70943260a3cac39ae3e727ad9f162f0
Christopher Acosta <chriz@live.dk>
2023-07-20 20:44:35 +0200

    Some cleanup.

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -2,30 +2,33 @@ use proc_macro::TokenStream;
 use proc_macro2::{Span, TokenTree};
 use quote::quote;
 use syn::{
-    parse_macro_input,
-    punctuated::Pair,
-    AngleBracketedGenericArguments, Data, DeriveInput, GenericArgument, Ident, Meta,
-    Path, PathArguments, PathSegment, Type, TypePath,
+    parse_macro_input, punctuated::Pair, AngleBracketedGenericArguments, Data, DeriveInput,
+    GenericArgument, Ident, Meta, Path, PathArguments, PathSegment, Type, TypePath,
 };
 
 #[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    //panic!("{:#?}", input);
-    let name = input.ident;
-    let builder_name = Ident::new(&format!("{}Builder", name), Span::call_site());
+
+    let struct_name = input.ident;
+    let builder_struct_name = Ident::new(&format!("{}Builder", struct_name), Span::call_site());
+
+    // Fields of struct.
     let fields = match input.data {
         Data::Struct(s) => s.fields,
-        _ => panic!("Only structs are supported"),
+        _ => {
+            return syn::Error::new(Span::call_site(), "Only structs are supported")
+                .to_compile_error()
+                .into()
+        }
     };
-    // let fields_named = match fields {
-    //     Fields::Named(f) => f,
-    //     _ => panic!("Only structs with named fields are supported"),
-    // };
 
+    // Field declarations of the builder type.
     let builder_field_decls = fields.iter().map(|field| {
         let field_ident = field.ident.as_ref().unwrap();
         let field_ty = &field.ty;
+
+        // If the field is already an Option, do not wrap another Option around it.
         if is_wrapper_of("Option", field_ty).is_some() {
             quote!(#field_ident: #field_ty)
         } else {
@@ -33,15 +36,18 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
     });
 
-    let builder_fields_init = fields.iter().map(|field| {
+    // Initial field values of builder.
+    let builder_field_inits = fields.iter().map(|field| {
         let field_ident = field.ident.as_ref().unwrap();
         quote!(#field_ident: None)
     });
 
-    let setters = fields.iter().map(|field| {
+    // Setter methods of builder.
+    let builder_setters = fields.iter().map(|field| {
         let field_ident = field.ident.as_ref().unwrap();
         let field_ty = &field.ty;
 
+        // If the field is an Option, its inner type is parameter to its setter.
         match is_wrapper_of("Option", field_ty) {
             Some(ty) => quote!(pub fn #field_ident(&mut self, #field_ident: #ty) -> &mut Self {
                 self.#field_ident = Some(#field_ident);
@@ -54,69 +60,68 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
     });
 
+    // Methods for specifying values one at a time.
     let repeated_field_decls = fields.iter().map(|field| {
         let field_ident = field.ident.as_ref().unwrap();
         let field_ty = &field.ty;
-        //panic!("{:#?}", field_ty);
 
+        // Parse attributes of field.
         if let Some(attr) = field.attrs.iter().next() {
+            // Error to return when the attribute is in the wrong format.
+            let expected_builder_list_err =
+                syn::Error::new_spanned(attr.meta.clone(), "expected `builder(each = \"...\")`")
+                    .to_compile_error();
+
+            // Inside the attribute, we expect `builder(each = ...)`, which is a meta list.
             let meta_list = match attr.meta {
                 Meta::List(ref meta_list) => meta_list,
-                _ => return quote!()
+                _ => return expected_builder_list_err,
             };
-            //panic!("{:#?}", field_ident.to_string());
-            let builder_path = meta_list.path.segments.first();
-            let is_builder_inert_attr =
-                matches![builder_path, Some(ps) if ps.ident == "builder"];
-            if is_builder_inert_attr {
-                let inner_ty = is_wrapper_of("Vec", field_ty).expect("expected type Vec<_>");
 
-                //panic!("{:#?}", attr);
-                let mut tokens: proc_macro2::token_stream::IntoIter = meta_list.tokens.clone().into_iter();
-                //let b1 = matches![tokens.next(), Some(TokenTree::Ident(i)) if i.to_string() == "each"];
-                let tkntree = tokens.next().unwrap();
-                match tkntree {
-                    TokenTree::Ident(ref i) if i.to_string() != "each" => {
-                        return syn::Error::new_spanned(attr.meta.clone(), "expected `builder(each = \"...\")`").to_compile_error();
-                    },
-                    _ => (),
-                }
-                if !false {
-                    //compile_error!("expected #[builder(each = ...)");
-                }
-                //let b2 = matches![tokens.next(), Some(TokenTree::Punct(p)) if p.as_char() == '='];
-                let tkntree = tokens.next().unwrap();
-                match tkntree {
-                    TokenTree::Punct(ref p) if p.as_char() != '=' => panic!("{:?}, {}", tkntree, p),
-                    _ => (),
-                }
-                let lit = match tokens.next() {
-                    Some(lit @ TokenTree::Literal(_)) => format!("{}", lit),
-                     _ => panic!(),
-                };
-                let each = lit.trim_matches('"');
-                let each_ident = Ident::new(&format!("{}", each), Span::call_site());
-                if each != field_ident.to_string() {
-                    let x: TokenStream = quote!(pub fn #each_ident(&mut self, #each_ident: #inner_ty) -> &mut Self {
-                        match self.#field_ident {
-                            Some(ref mut v) => v.push(#each_ident),
-                            None => self.#field_ident = Some(vec![#each_ident]),
-                        }
-                        self
-                    }).into();
-                    //panic!("{}", x.to_string());
-                    x.into()
-                } else {
-                    quote!()
-                }
+            // The path of the meta list is the `builder` of `builder(each = ...)`.
+            let builder_path = meta_list.path.segments.first();
+
+            // I think this is guaranteed to be true, as we can't refer to any attributes not specified in the proc macro declaration.
+            assert!(matches![builder_path, Some(ps) if ps.ident == "builder"]);
+
+            // Fields annotated with the attribute is of type Vec.
+            let inner_ty = is_wrapper_of("Vec", field_ty).expect("field type should be Vec<_>");
+
+            let tokens: Vec<TokenTree> =
+                meta_list.tokens.clone().into_iter().collect();
+            if tokens.len() != 3 {
+                return expected_builder_list_err;
+            }
+
+            // Check for `each` and `=`.
+            let each = &tokens[0];
+            if each.to_string() != "each" {
+                return expected_builder_list_err;
+            }
+            let eq = &tokens[1];
+            if eq.to_string() != "=" {
+                return expected_builder_list_err;
+            }
+            
+            // If the repeated method name equals the field name, do nothing.
+            let repeated_method_name = Ident::new((&tokens[2]).to_string().trim_matches('"'), Span::call_site());
+            if repeated_method_name != field_ident.to_string() {
+                quote!(pub fn #repeated_method_name(&mut self, #repeated_method_name: #inner_ty) -> &mut Self {
+                    match self.#field_ident {
+                        Some(ref mut v) => v.push(#repeated_method_name),
+                        None => self.#field_ident = Some(vec![#repeated_method_name]),
+                    }
+                    self
+                })
             } else {
-                panic!()
+                quote!()
             }
         } else {
             quote!()
         }
     });
 
+    // Build the field values.
     let build_checks = fields.iter().map(|field| {
         let field_ident = field.ident.as_ref().unwrap();
         let field_ty = &field.ty;
@@ -137,26 +142,26 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
     });
 
-    let build = fields.iter().map(|field| field.ident.as_ref().unwrap());
+    let builder_instantiation_values = fields.iter().map(|field| field.ident.as_ref().unwrap());
 
     let expanded = quote! {
-        pub struct #builder_name {
+        pub struct #builder_struct_name {
             #(#builder_field_decls),*
         }
-        impl #builder_name {
-            pub fn build(&mut self) -> Result<#name, Box<dyn std::error::Error>> {
+        impl #builder_struct_name {
+            pub fn build(&mut self) -> Result<#struct_name, Box<dyn std::error::Error>> {
                 #(#build_checks)*
-                Ok(#name {
-                    #(#build),*
+                Ok(#struct_name {
+                    #(#builder_instantiation_values),*
                 })
             }
-            #(#setters)*
+            #(#builder_setters)*
             #(#repeated_field_decls)*
         }
-        impl #name {
-            pub fn builder() -> #builder_name {
-                #builder_name {
-                    #(#builder_fields_init),*
+        impl #struct_name {
+            pub fn builder() -> #builder_struct_name {
+                #builder_struct_name {
+                    #(#builder_field_inits),*
                 }
             }
         }
@@ -164,6 +169,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
     expanded.into()
 }
 
+/// Test if `field_ty` is of wrapper type `wrapper_ty`, e.g., Option or Vec.
 fn is_wrapper_of(wrapper_ty: &str, field_ty: &Type) -> Option<Type> {
     let segments = match field_ty {
         Type::Path(TypePath {
@@ -186,4 +192,3 @@ fn is_wrapper_of(wrapper_ty: &str, field_ty: &Type) -> Option<Type> {
         _ => None,
     }
 }
-
