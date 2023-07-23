b18e8267c28a51c8e665336de7bc093b5ea12a3c
Christopher Acosta <chriz@live.dk>
2023-07-23 17:44:37 +0200

    DeriveDebug pt. 4

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,5 +1,5 @@
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput, Data, DataStruct, FieldsNamed, Fields, Field, Expr, Lit, ExprLit, spanned::Spanned};
+use syn::{parse_macro_input, DeriveInput, Data, DataStruct, FieldsNamed, Fields, Field, Expr, Lit, ExprLit, Generics, GenericParam, parse_quote};
 use quote::quote;
 
 #[proc_macro_derive(CustomDebug, attributes(debug))]
@@ -35,9 +35,14 @@ pub fn derive(input: TokenStream) -> TokenStream {
         )
     });
 
+    let generics = add_trait_bounds(ast.generics);
+    //eprintln!("{:#?}", generics);
+
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
     let name_str = name.to_string();
     let expanded = quote!(
-        impl std::fmt::Debug for #name {
+        impl #impl_generics std::fmt::Debug for #name #ty_generics #where_clause {
             fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
                 fmt.debug_struct(#name_str)
                 #(#add_debug_fields)*
@@ -50,7 +55,6 @@ pub fn derive(input: TokenStream) -> TokenStream {
 }
 
 fn debug_attr_format(f: &Field) -> Option<proc_macro2::TokenStream> {
-    eprintln!("{:#?}", f);
     if f.attrs.is_empty() {
         return None;
     }
@@ -81,4 +85,13 @@ fn debug_attr_format(f: &Field) -> Option<proc_macro2::TokenStream> {
     let format_ts = quote!(&format_args!(#format, &self.#name));
 
     Some(format_ts)
+}
+
+fn add_trait_bounds(mut generics: Generics) -> Generics {
+    for param in &mut generics.params {
+        if let GenericParam::Type(ref mut ty_param) = param {
+            ty_param.bounds.push(parse_quote!(std::fmt::Debug));
+        }
+    }
+    generics
 }
