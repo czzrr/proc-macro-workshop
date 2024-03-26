188490a264df810429fec66e9c71130ada57f1a3
Christopher Acosta <chriz@live.dk>
2024-03-26 10:43:32 +0100

    sorted: part 5

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,25 +1,29 @@
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
-use syn::parse_macro_input;
+use quote::quote;
 use syn::spanned::Spanned;
-use syn::Item;
+use syn::visit_mut::VisitMut;
+use syn::{parse_macro_input, ItemFn};
+use syn::{Item, Pat};
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
-    //println!("args: {:?}", args);
     let _ = args;
     let _ = input;
-    //println!("input: {:#?}", input);
     let x = input.clone();
     let item = parse_macro_input!(x as Item);
-    //sorted_helper(item).unwrap_or_else(syn::Error::into_compile_error).into()
     match sorted_helper(item) {
         Ok(_) => input,
-        Err(err) => syn::Error::into_compile_error(err).into()
+        Err(err) => {
+            let err = syn::Error::into_compile_error(err);
+            let mut input = input;
+            input.extend::<TokenStream>(err.into());
+            input
+        }
     }
 }
 
-fn sorted_helper(item: Item) -> syn::Result<TokenStream2> {
+fn sorted_helper(item: Item) -> syn::Result<()> {
     match item {
         Item::Enum(item) => {
             let variants: Vec<_> = item.variants.into_iter().collect();
@@ -29,11 +33,11 @@ fn sorted_helper(item: Item) -> syn::Result<TokenStream2> {
                         return Err(syn::Error::new(
                             variants[i].span(),
                             format!("{} should sort before {}", variants[i].ident, v.ident),
-                        )) 
+                        ));
                     }
                 }
             }
-        },
+        }
         _ => {
             return Err(syn::Error::new(
                 proc_macro::Span::call_site().into(),
@@ -42,10 +46,68 @@ fn sorted_helper(item: Item) -> syn::Result<TokenStream2> {
         }
     }
 
-    Ok(TokenStream::new().into())
+    Ok(())
+}
+
+struct FnMatchSortedCheck {
+    violator: Option<TokenStream2>,
+}
+
+impl FnMatchSortedCheck {
+    pub fn new() -> Self {
+        FnMatchSortedCheck { violator: None }
+    }
+}
+
+impl VisitMut for FnMatchSortedCheck {
+    fn visit_expr_match_mut(&mut self, item: &mut syn::ExprMatch) {
+        let found = item
+            .attrs
+            .iter()
+            .position(|a| a.path().get_ident().unwrap() == "sorted");
+        if let Some(idx) = found {
+            item.attrs.remove(idx);
+        }
+        let mut v = Vec::new();
+        for arm in &item.arms {
+            match &arm.pat {
+                Pat::TupleStruct(pat) => {
+                    let s = pat.path.segments.first().unwrap().ident.to_string();
+                    v.push((s, pat.span()))
+                }
+                _ => (),
+            }
+            for i in 0..v.len() {
+                for (pat1, _) in &v[..i] {
+                    let (pat2, span2) = &v[i];
+                    if let std::cmp::Ordering::Less = pat2.cmp(pat1) {
+                        self.violator = Some(
+                            syn::Error::new(
+                                span2.clone(),
+                                format!("{} should sort before {}", pat2, pat1),
+                            )
+                            .into_compile_error(),
+                        );
+                    }
+                }
+            }
+        }
+    }
 }
 
 #[proc_macro_attribute]
 pub fn check(args: TokenStream, input: TokenStream) -> TokenStream {
-    TokenStream::new()
+    let _ = args;
+    let _ = input;
+    let mut item_fn = parse_macro_input!(input as ItemFn);
+    let mut check = FnMatchSortedCheck::new();
+    check.visit_item_fn_mut(&mut item_fn);
+    if let Some(err) = check.violator {
+        quote!(
+            #item_fn
+            #err)
+        .into()
+    } else {
+        quote!(#item_fn).into()
+    }
 }
--- sorted/src/main.rs
+++ sorted/src/main.rs
@@ -66,28 +66,4 @@
 //   - The ExprMatch struct:
 //     https://docs.rs/syn/1.0/syn/struct.ExprMatch.html
 
-use sorted::sorted;
-
-use std::fmt::{self, Display};
-use std::io;
-
-#[sorted]
-pub enum Error {
-    Fmt(fmt::Error),
-    Io(io::Error),
-}
-
-impl Display for Error {
-    #[sorted::check]
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        use self::Error::*;
-
-        #[sorted]
-        match self {
-            Io(e) => write!(f, "{}", e),
-            Fmt(e) => write!(f, "{}", e),
-        }
-    }
-}
-
 fn main() {}
